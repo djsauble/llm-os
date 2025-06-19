1df753eb0c29e9d9c47f9a170864a1e89bbf3fb8
Daniel Sauble <djsauble@gmail.com>
2025-06-19 14:19:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# LMM OS
+
+This is a design concept of an operating system based around LLMs and agentic workflows
+
+## Setup
+
+```
+npm install
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Home } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <Home fontSize='large'/>
+      <Typography variant="h1" component="h2" gutterBottom>Hello, world!</Typography>  
+    </>
+  )
+}
+
+export default App
