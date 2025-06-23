c96856aed0a1c20ab9408ef2d885eda55655ce92
Daniel Sauble <djsauble@gmail.com>
2025-06-23 09:24:12 -0700

    Wire up the nav bar so that the active nav item changes based on the route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,21 +1,27 @@
-import { Radio } from '@base-ui-components/react/radio';
-import { RadioGroup } from '@base-ui-components/react/radio-group';
+import { Link, useLocation } from 'react-router-dom';
 import { Command, Plus } from 'react-feather';
 
 function Sidebar() {
+  const location = useLocation();
+
   return (
     <div className="bg-slate-50 p-4 flex flex-col">
-      <RadioGroup
-        defaultValue="home"
-        className="flex md:flex-col max-md:space-x-4 md:space-y-4 items-center"
-      >
-        <Radio.Root value="home" aria-label="Home" className="btn-icon p-4">
+      <nav className="flex md:flex-col max-md:space-x-4 md:space-y-4 items-center">
+        <Link
+          to="/"
+          aria-label="Home"
+          className={`btn-icon p-4 ${location.pathname === '/' ? 'bg-gray-200 text-gray-800 shadow-inner border-gray-300' : ''}`}
+        >
           <Command className="w-6 h-6" />
-        </Radio.Root>
-        <Radio.Root value="definition" aria-label="Task Definition" className="btn-icon p-4">
+        </Link>
+        <Link
+          to="/definition"
+          aria-label="Task Definition"
+          className={`btn-icon p-4 ${location.pathname === '/definition' ? 'bg-gray-200 text-gray-800 shadow-inner border-gray-300' : ''}`}
+        >
           <Plus className="w-6 h-6" />
-        </Radio.Root>
-      </RadioGroup>
+        </Link>
+      </nav>
     </div>
   );
 }
