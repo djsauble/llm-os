0739a81c60b13cb2e56f96628a08741b27e040e2
Daniel Sauble <djsauble@gmail.com>
2025-06-23 09:17:13 -0700

    Add a new affordance in the nav bar for the Definition page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { Radio } from '@base-ui-components/react/radio';
 import { RadioGroup } from '@base-ui-components/react/radio-group';
-import { Command } from 'react-feather';
+import { Command, Plus } from 'react-feather';
 
 function Sidebar() {
   return (
@@ -11,6 +11,9 @@ function Sidebar() {
       >
         <Radio.Root value="home" aria-label="Home" className="btn-icon p-4">
           <Command className="w-6 h-6" />
+        </Radio.Root>
+        <Radio.Root value="definition" aria-label="Task Definition" className="btn-icon p-4">
+          <Plus className="w-6 h-6" />
         </Radio.Root>
       </RadioGroup>
     </div>
